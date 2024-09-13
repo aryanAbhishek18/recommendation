1fc9c0c8905586c9cae32a0ec9abc29fb2783ebc
Abhishek Aryan <aryan.abhishek1829@gmail.com>
2024-09-13 23:06:42 +0530

    Added entity, repository, dto and mapper repos

---
+++ src/main/java/com/aryanabhi/recommendation/dto/CarDto.java
@@ -0,0 +1,43 @@
+package com.aryanabhi.recommendation.dto;
+
+public class CarDto {
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    public CarDto() {
+    }
+
+    public CarDto(Long id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
---
+++ src/main/java/com/aryanabhi/recommendation/entity/Car.java
@@ -0,0 +1,52 @@
+package com.aryanabhi.recommendation.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="car",
+        indexes = {@Index(name = "type_index",  columnList="type")})
+public class Car {
+
+    public Car() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "type", nullable = false)
+    private String type;
+
+    public Car(Long id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}

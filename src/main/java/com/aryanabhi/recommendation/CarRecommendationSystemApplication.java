3d880f7a85c552b5d2d7afe6ce86e1bc745c3b7c
Abhishek Aryan <aryan.abhishek1829@gmail.com>
2024-09-13 22:58:36 +0530

    Added Spring Boot boilerplate code

---
+++ src/main/java/com/aryanabhi/recommendation/CarRecommendationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.aryanabhi.recommendation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarRecommendationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarRecommendationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aryanabhi/recommendation/CarRecommendationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aryanabhi.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarRecommendationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

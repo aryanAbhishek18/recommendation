5827cd5a0e13b9f441e21586d5dd3eea0b749e26
Abhishek Aryan <aryan.abhishek1829@gmail.com>
2024-09-17 14:35:11 +0530

    Changed the comparisons api to POST

--- src/main/java/com/aryanabhi/recommendation/controller/ComparisonController.java
+++ src/main/java/com/aryanabhi/recommendation/controller/ComparisonController.java
@@ -8,7 +8,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,7 +27,7 @@ public class ComparisonController {
         this.comparisonService = comparisonService;
     }
 
-    @GetMapping
+    @PostMapping
     public ResponseEntity<ComparisonResponseDto> compareCars(@RequestBody ComparisonRequestDto req) {
         log.debug("Request to fetch comparisons for cars with ids: {}", req.getIds());
         try {
